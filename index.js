5d1818f99020443dd2292e348ce21cdefca79bbc
lfpborba <luisfpb@icloud.com>
2022-08-02 19:14:57 -0300

    Requirement 01 - Done

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const express = require('express');
+
 const bodyParser = require('body-parser');
+
+const data = require('./data.js');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
-const PORT = '3000';
+const PORT = 3000;
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
+  response.status(HTTP_OK_STATUS).json();
+});
+
+app.get('/talker', (req, res) => {
+  res.status(HTTP_OK_STATUS).json(data);
 });
 
 app.listen(PORT, () => {
   console.log('Online');
 });
-
