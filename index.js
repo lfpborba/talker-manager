d1c9f5493bb8970cb3b5dc0024587fabca279b3b
lfpborba <luisfpb@icloud.com>
2022-08-03 13:44:33 -0300

    Restarting and fixing req1

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const express = require('express');
 
-const bodyParser = require('body-parser');
+const fs = require('fs');
 
-const data = require('./data.js');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
-const PORT = 3000;
+const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).json();
+  response.status(HTTP_OK_STATUS).send();
 });
 
 app.get('/talker', (req, res) => {
-  res.status(HTTP_OK_STATUS).json(data);
-});
+  fs.readFile('./talker.json', (err, data) => {
+   const talker = JSON.parse(data);
+   if (err) return res.status(404).json(talker);
+   res.status(HTTP_OK_STATUS).json(talker);
+  });
+ });
 
 app.listen(PORT, () => {
   console.log('Online');
